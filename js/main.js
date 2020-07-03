a09958e9a92829fa144efb9caf2ac8b96bc2514b
Victoria Marques <vicck.rj@gmail.com>
2020-07-02 22:37:01 -0300

    codigo não finalizado

--- js/main.js
+++ js/main.js
@@ -2,26 +2,29 @@ let data = document.querySelector('#data');
 let imagem = document.querySelector('#imagem');
 let descricao = document.querySelector('#descricao');
 
+
+console.log("oi")
 // requisição API
 
 let reqImagem = new XMLHttpRequest();
 
 reqImagem.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=Ybsgh0M67YWj8jz90WZHMAHc2eLtGBSrRal69AB5');
 
-reqImagem.send();
 
 reqImagem.addEventListener('load', function(){
 
     let reqImagemParse = JSON.parse(reqImagem.responseText);
-    let responseData = new Date(reqImagemParse.date);
+    let responseData = reqImagemParse.date;
     let responseImagem = reqImagemParse.url;
     let responseDescricao = reqImagemParse.explanation;
 
-    data.textContent = responseData.toDateString("MMM DD YYYY");
+    data.textContent = responseData;
     descricao.textContent = responseDescricao;
     imagem.src = responseImagem;
 
 })
+
+reqImagem.send();
 
 
 
